4eeccc27721b81f705dd0763126ff5aaed915046
Patrick Robertson <patricksrobertson@gmail.com>
2025-03-07 16:05:37 -0500

    Extract mission object to another file.

--- src/app/components/MissionFilter.tsx
+++ src/app/components/MissionFilter.tsx
@@ -1,8 +1,9 @@
-"use client";
+'use client';
 
-import React from "react";
-import { useState } from "react";
-import FilterItem from "./FilterItem";
+import React from 'react';
+import { useState } from 'react';
+import FilterItem from './FilterItem';
+import { missions } from './missions';
 
 export default function MissionFilter() {
   const [longBoard, setLongBoard] = useState(false);
@@ -16,154 +17,11 @@ export default function MissionFilter() {
   const [layoutEightMissions, setLayoutEightMissions] = useState(true);
   const [ritual, setRitual] = useState(false);
 
-  const missions = [
-    {
-      name: "A",
-      primary: "Take and Hold",
-      missionRule: "Raise Banners",
-      deployment: "Tipping Point",
-      layouts: [1, 2, 4, 6, 7, 8],
-    },
-    {
-      name: "B",
-      primary: "Purge the Foe",
-      missionRule: "Smoke and Mirrors",
-      deployment: "Tipping Point",
-      layouts: [1, 2, 4, 6, 7, 8],
-    },
-    {
-      name: "C",
-      primary: "Linchpin",
-      missionRule: "Fog of War",
-      deployment: "Tipping Point",
-      layouts: [1, 2, 4, 6, 7, 8],
-    },
-    {
-      name: "D",
-      primary: "Scorched Earth",
-      missionRule: "Swift Action",
-      deployment: "Tipping Point",
-      layouts: [1, 2, 4, 6, 7, 8],
-    },
-    {
-      name: "E",
-      primary: "Take and Hold",
-      missionRule: "Prepared Positions",
-      deployment: "Hammer and Anvil",
-      layouts: [1, 7, 8],
-    },
-    {
-      name: "F",
-      primary: "Burden of Trust",
-      missionRule: "Hidden Supplies",
-      deployment: "Hammer and Anvil",
-      layouts: [1, 7, 8],
-    },
-    {
-      name: "G",
-      primary: "The Ritual",
-      missionRule: "Stalwarts",
-      deployment: "Hammer and Anvil",
-      layouts: [1, 7, 8],
-    },
-    {
-      name: "H",
-      primary: "Supply Drop",
-      missionRule: "Smoke and Mirrors",
-      deployment: "Hammer and Anvil",
-      layouts: [1, 7, 8],
-    },
-    {
-      name: "I",
-      primary: "Burden of Trust",
-      missionRule: "Prepared Positions",
-      deployment: "Search and Destroy",
-      layouts: [1, 2, 3, 4, 6],
-    },
-    {
-      name: "J",
-      primary: "Linchpin",
-      missionRule: "Raise Banners",
-      deployment: "Search and Destroy",
-      layouts: [1, 2, 3, 4, 6],
-    },
-    {
-      name: "K",
-      primary: "Scorched Earth",
-      missionRule: "Stalwarts",
-      deployment: "Search and Destroy",
-      layouts: [1, 2, 3, 4, 6],
-    },
-    {
-      name: "L",
-      primary: "Take and Hold",
-      missionRule: "Hidden Supplies",
-      deployment: "Search and Destroy",
-      layouts: [1, 2, 3, 4, 6],
-    },
-    {
-      name: "M",
-      primary: "Purge the Foe",
-      missionRule: "Rapid Escalation",
-      deployment: "Crucible of Battle",
-      layouts: [1, 2, 4, 6, 8],
-    },
-    {
-      name: "N",
-      primary: "The Ritual",
-      missionRule: "Swift Action",
-      deployment: "Crucible of Battle",
-      layouts: [1, 2, 4, 6, 8],
-    },
-    {
-      name: "O",
-      primary: "Terraform",
-      missionRule: "Stalwarts",
-      deployment: "Crucible of Battle",
-      layouts: [1, 2, 4, 6, 8],
-    },
-    {
-      name: "P",
-      primary: "Scorched Earth",
-      missionRule: "Inspired Leadership",
-      deployment: "Crucible of Battle",
-      layouts: [1, 2, 4, 6, 8],
-    },
-    {
-      name: "Q",
-      primary: "Supply Drop",
-      missionRule: "Rapid Escalation",
-      deployment: "Sweeping Engagement",
-      layouts: [3, 5],
-    },
-    {
-      name: "R",
-      primary: "Terraform",
-      missionRule: "Swift Action",
-      deployment: "Sweeping Engagement",
-      layouts: [3, 5],
-    },
-    {
-      name: "S",
-      primary: "Linchpin",
-      missionRule: "Raise Banners",
-      deployment: "Dawn of War",
-      layouts: [5],
-    },
-    {
-      name: "T",
-      primary: "Unexploded Ordnance",
-      missionRule: "Inspired Leadership",
-      deployment: "Dawn of War",
-      layouts: [5],
-    },
-  ];
-
   const filteredMissions = missions.filter((mission) => {
     if (
       longBoard &&
-      (mission.deployment == "Dawn of War" ||
-        mission.deployment == "Sweeping Engagement")
+      (mission.deployment == 'Dawn of War' ||
+        mission.deployment == 'Sweeping Engagement')
     ) {
       return false;
     } else if (!layoutOneMissions && mission.layouts.includes(1)) {
@@ -181,7 +39,7 @@ export default function MissionFilter() {
       return false;
     } else if (!layoutEightMissions && mission.layouts.includes(8)) {
       return false;
-    } else if (!ritual && mission.primary == "The Ritual") {
+    } else if (ritual && mission.primary == 'The Ritual') {
       return false;
     } else {
       return mission;
@@ -191,7 +49,7 @@ export default function MissionFilter() {
   const missionList = filteredMissions.map((mission) => {
     return (
       <li key={mission.name}>
-        Misson {mission.name}: {mission.primary} | {mission.missionRule} |{" "}
+        Misson {mission.name}: {mission.primary} | {mission.missionRule} |{' '}
         {mission.deployment}
       </li>
     );
